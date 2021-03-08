52e75873bcd9bc2d8b5caeb2ad4704ac372a0e61
阿昆 <37210982+hqqkun@users.noreply.github.com>
2021-03-08 14:44:58 +0800

    Create sleep.c

---
+++ lab util/sleep.c
@@ -0,0 +1,22 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc,char* argv[])
+{
+	if(argc == 1){
+		fprintf(2,"you need to tell a time to sleep.\n");
+		exit(1);
+		}
+	else if(argc >= 3){
+		fprintf(2,"too much arguments.\n");
+		exit(1);
+		}
+		
+	else{
+		/* change string into integer */
+		sleep(atoi(argv[1]));
+		exit(0);
+		}
+}
+		
