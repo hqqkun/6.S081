4e845bc46d201f6daa1f959180f6abaf5928d91b
阿昆 <37210982+hqqkun@users.noreply.github.com>
2021-09-23 21:03:19 +0800

    Update sleep.c

--- Utilities/sleep.c
+++ Utilities/sleep.c
@@ -1,22 +1,15 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
-   
-int main(int argc,char* argv[])
+
+
+int main(int argc, char const *argv[])
 {
-	if(argc == 1){
-		fprintf(2,"you need to tell a time to sleep.\n");
+	if (argc < 2) {
+		fprintf(2, "Usage: time\n");
 		exit(1);
-		}
-	else if(argc >= 3){
-		fprintf(2,"too much arguments.\n");
-		exit(1);
-		}
-		
-	else{
-		/* change string into integer */
-		sleep(atoi(argv[1]));
-		exit(0);
-		}
+	}
+	
+	// xv6 rewrite atoi, so only nonnegative numbers.
+	sleep(atoi(argv[1]));
+	exit(0);
 }
-		
