77b726fb65a0cca4db8eb13f89570e478447ce99
阿昆 <37210982+hqqkun@users.noreply.github.com>
2021-09-23 23:25:18 +0800

    Update primes.c

--- Utilities/primes.c
+++ Utilities/primes.c
@@ -1,5 +1,4 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
  
 #define R 0
@@ -13,15 +12,15 @@ int main(int argc, char const *argv[]) {
 		exit(1);
 	if (!fork()) {
 		// child process
-		redirect(R,write_pipe);
+		redirect(R, write_pipe);
 		pipelineprime(R);
 		exit(0);
 	}
 
 	// father process
-	redirect(W,write_pipe);
+	redirect(W, write_pipe);
 	for (int num = 2; num != 36; ++num)
-		write(W,&num,sizeof(int));
+		write(W, (void*)&num, sizeof(int));
 
 	close(W);
 	wait((int*)0);
@@ -29,7 +28,7 @@ int main(int argc, char const *argv[]) {
 }
 
 /* match stdin or stdout to pipe fd and close them. */
-void redirect(int k,int pipe[]) {
+void redirect(int k, int pipe[]) {
 	close(k);
 	dup(pipe[k]);
 	close(pipe[R]);
@@ -42,18 +41,18 @@ void redirect(int k,int pipe[]) {
    receive at least one number from it's father by reading fd */
 void pipelineprime(int fd) {
 	/* stage_prime means other numbers will be sieved by it. */
-	int stage_prime,prime;
+	int stage_prime, prime;
 	/* used to transmit numbers to it's child */
 	int write_pipe[2];
 	int prime_count = 0;
 	/* check if generate a pipe */
 	int pipe_flag = 0;				
-	read(fd,&stage_prime,sizeof(int));
+	read(fd, &stage_prime, sizeof(int));
 
-	printf("prime %d\n",stage_prime);
+	printf("prime %d\n", stage_prime);
 
 	/* read until there is nothing to read */
-	while (read(fd,&prime,sizeof(int)) != 0 ) {
+	while (read(fd, &prime, sizeof(int)) != 0 ) {
 		if ((prime % stage_prime) != 0) {
 			// might find a prime
 			++prime_count;
@@ -67,14 +66,14 @@ void pipelineprime(int fd) {
 				
 				if (!fork()) {
 					/* child */
-					redirect(R,write_pipe);
+					redirect(R, write_pipe);
 					pipelineprime(R);
 					exit(0);
 				}
 				/* father */
-				redirect(W,write_pipe);
+				redirect(W, write_pipe);
 			}
-			write(W,&prime,sizeof(int));		
+			write(W, &prime, sizeof(int));		
 		}
 	}
 	close(fd);
