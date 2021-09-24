82aef762346f621a77219d619499e13bf5f7da03
阿昆 <37210982+hqqkun@users.noreply.github.com>
2021-09-24 12:43:25 +0800

    Update find.c

--- Utilities/find.c
+++ Utilities/find.c
@@ -3,98 +3,74 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-#define MaxPathSize 512
-#define MaxNameSize 64
+/* buf will store the path of the file. */
+static char buf[512] = {0};
 
-static char*
-fmt_name(const char *path)
-{
-	/* 1 for '\0' */
-  	static char buf[MaxNameSize + 1];
-  	int length;
-  	const char *p;
+static void find(char*, const char*);
+static char* fmtname(char*);
 
-  	// Find first character after last slash.
-  	for(p = path + strlen(path); p >= path && *p != '/'; --p)
-    	;
-  	++p;
+int main(int argc, char const *argv[]) {
+	if (argc < 2) {
+		fprintf(2, "Usage: find filename\n");
+		exit(1);
+	}
 
-  	length = strlen(p);
-  	if(length > MaxNameSize)
-    	return (char*)0;
-	// 1  for '\0' 
-  	memmove(buf, p, length + 1);
-  	return buf;
+	strcpy(buf, argv[1]);
+	find(buf, argv[2]);
+	exit(0);
 }
 
-void 
-find(const char* path,const char* filename)
-{
-	char* buf = (char*)malloc(MaxPathSize * sizeof(char));
+
+static void find(char* path, const char* fileName) {
 	char* p;
 	int fd;
-	/* the total path length */ 
-	int length;	
 	struct dirent de;
-	struct stat st;
+  	struct stat st;
 
-	if((fd = open(path,0)) < 0){
-		fprintf(2, "find: cannot open %s\n",path);
-		return;
-	}
+  	if((fd = open(path, 0)) < 0){
+    		fprintf(2, "find: cannot open %s\n", path);
+    		return;
+  	}
+	if(fstat(fd, &st) < 0){
+    		fprintf(2, "find: cannot stat %s\n", path);
+    		close(fd);
+    		return;
+  	}
 
-	if(fstat(fd,&st) < 0){
-		fprintf(2, "find: cannot stat %s\n",path);
-		close(fd);
-		return;
-	}
-	switch(st.type){
-		case T_FILE:
-			/* check the current filename with the specific file. */
-			if(!strcmp(fmt_name(path),filename))
-				printf("%s\n",path);
-			break;
-
-		case T_DIR:
-			/* check the current directory */
-			/* 1 for '/' and 1 for '\0' */
-			if(strlen(path) + 1 + MaxNameSize + 1 > (MaxPathSize * sizeof(char))){
-				printf("find: path too long.\n");
-				break;
-			}
-
-			strcpy(buf,path);
-			p = buf + strlen(buf);
-			*p++ = '/';
-
-			while(read(fd,&de,sizeof(de)) == sizeof(de)){
+  	switch (st.type) {
+  		case T_FILE:
+  			if (strcmp(fmtname(path), fileName) == 0) {
+  				fprintf(1, "%s\n", path);
+  			}
+  			break;
+  		case T_DIR:
+  			p = buf + strlen(buf);
+  			*p++ = '/';
+  			while (read(fd, &de, sizeof(de)) == sizeof(de)) {
 				/* we don't recurse into "." and ".." */
-				if(de.inum == 0 || !strcmp(de.name,".") || !strcmp(de.name,".."))
-					continue;
-				length = strlen(de.name);
-
-				memmove(p,de.name,length);
-				p[length] = 0;
-				if(stat(buf,&st) < 0){
-					printf("find: cannot stat %s\n",buf);
-					continue;
-				}
-				find(buf,filename);
-			}
-			break;
-
-	}
-	free(buf);
-	close(fd);
+  				if (de.inum == 0) 
+  					continue;
+  				if (strcmp(de.name, ".") == 0)
+  					continue;
+  				if (strcmp(de.name, "..") == 0) 
+  					continue;
+  				int length = strlen(de.name);
+  				memmove(p, de.name, length);
+  				p[length] = 0;
+  				find(buf, fileName);
+  			}
+  			break;
+  	}	
+  	close(fd);
+  	return;
 }
 
-int
-main(int argc, char *argv[])
-{
-  if(argc != 3){
-  	fprintf(2, "format:<path> <filename>\n");
-    exit(1);
-  }
-  find(argv[1],argv[2]);
-  exit(0);
+static char* fmtname(char *path) {
+	char *p;
+
+  	// Find first character after last slash.
+  	for (p = path+strlen(path); p >= path && *p != '/'; p--)
+    	;
+  	p++;
+  	return p;
 }
