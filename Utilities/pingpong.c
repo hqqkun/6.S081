50f538d0a976385321a1d84ebb3c36eda647ff2f
阿昆 <37210982+hqqkun@users.noreply.github.com>
2021-09-24 10:15:27 +0800

    Update pingpong.c

--- Utilities/pingpong.c
+++ Utilities/pingpong.c
@@ -4,6 +4,14 @@
 #define R 0
 #define W 1
 
+
+static inline void reDirect(int direction, int* pipe) {
+	close(direction);
+	dup(pipe[direction]);
+	close(pipe[R]);
+	close(pipe[W]);
+}
+
 int main(int argc, char const *argv[]) {
 	int ping[2];
 	int pong[2];
@@ -13,7 +21,6 @@ int main(int argc, char const *argv[]) {
 	if (pipe(pong) != 0) {
 		exit(1);
 	} 
-
 	int childId = fork();
 	if (childId < 0) {
 		exit(1);
@@ -21,24 +28,32 @@ int main(int argc, char const *argv[]) {
 	if (!childId) {
 		// child process
 		char cc;
-		read(ping[R], (void*)&cc, 1);
-		printf("%d: received ping\n", getpid());
+
+		reDirect(R, ping);
+		reDirect(W, pong);
+		
+		read(R, (void*)&cc, 1);
+		fprintf(2, "%d: received ping\n", getpid());
 		cc = 'C';
-		write(pong[W], (void*)&cc, 1);
 
-		close(ping[R]);
-		close(pong[W]);
+		write(W, (void*)&cc, 1);
 
+		close(R);
+		close(W);
 		exit(0);
 	}
 	// parent process
 	char c = 'P';
-	write(ping[W], (void*)&c, 1);
-	read(pong[R], (void*)&c, 1);
-	printf("%d: received pong\n", getpid());
 
-	close(ping[W]);
-	close(pong[R]);
-	
+	reDirect(W, ping);
+	reDirect(R, pong);
+
+	write(W, (void*)&c, 1);
+	read(R, (void*)&c, 1);
+	fprintf(2, "%d: received pong\n", getpid());
+
+	wait(0);
+	close(R);
+	close(W);
 	exit(0);
 }
