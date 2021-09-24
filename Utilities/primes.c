dc38354e7e9f95fa03a53c493a58dbb324bf0754
阿昆 <37210982+hqqkun@users.noreply.github.com>
2021-09-24 16:36:16 +0800

    Update primes.c

--- Utilities/primes.c
+++ Utilities/primes.c
@@ -1,83 +1,82 @@
 #include "kernel/types.h"
 #include "user/user.h"
- 
+
 #define R 0
 #define W 1
-void pipelineprime(int fd);
-void redirect(int k,int* pipe);
 
-int main(int argc, char const *argv[]) {
-	int write_pipe[2];
-	if (pipe(write_pipe) < 0)
-		exit(1);
-	if (!fork()) {
-		// child process
-		redirect(R, write_pipe);
-		pipelineprime(R);
-		exit(0);
-	}
-
-	// father process
-	redirect(W, write_pipe);
-	for (int num = 2; num != 36; ++num)
-		write(W, (void*)&num, sizeof(int));
-
-	close(W);
-	wait((int*)0);
-	exit(0);
-}
-
-/* match stdin or stdout to pipe fd and close them. */
-void redirect(int k, int pipe[]) {
-	close(k);
-	dup(pipe[k]);
+void reDirect(int direction, int *pipe) {
+	close(direction);
+	dup(pipe[direction]);
 	close(pipe[R]);
 	close(pipe[W]);
 }
 
-/* a recursive function that will fork a child 
-   process to sieve the next prime number. */
-/* we guarantee the current process will 
-   receive at least one number from it's father by reading fd */
-void pipelineprime(int fd) {
-	/* stage_prime means other numbers will be sieved by it. */
-	int stage_prime, prime;
-	/* used to transmit numbers to it's child */
-	int write_pipe[2];
-	int prime_count = 0;
-	/* check if generate a pipe */
-	int pipe_flag = 0;				
-	read(fd, &stage_prime, sizeof(int));
+void pipelinePrime(int fd);
 
-	printf("prime %d\n", stage_prime);
+int main(void) {
+	// process gen 2 ~ 35
+	int primePipe[2];
+	if (pipe(primePipe) < 0)
+		exit(1);
 
-	/* read until there is nothing to read */
-	while (read(fd, &prime, sizeof(int)) != 0 ) {
-		if ((prime % stage_prime) != 0) {
-			// might find a prime
-			++prime_count;
-			if (prime_count == 1) {
-				/* fork a child process only if we find the first 
-				   number that cannot divided by stage_prime. */ 
-				if (pipe(write_pipe) < 0)
+	int childPid = fork();
+	if (childPid < 0)
+		exit(1);
+
+	if (childPid) {
+		// parent process
+		reDirect(W, primePipe);
+		for (int i = 2; i != 36; ++i)
+			write(W, (void*)&i, sizeof(int));
+
+		close(W);
+		wait((int*)0);
+		exit(0);
+	}
+	else {
+		// child process
+		reDirect(R, primePipe);
+		pipelinePrime(R);
+		exit(0);
+	}
+}
+
+void pipelinePrime(int fd) {
+	// read from fd
+	int sieve, readNum;
+	int primePipe[2];
+	int primeCount = 0;
+	int pipeCreateFlag = 0;
+
+	read(fd, (void*)&sieve, sizeof(int));
+	fprintf(2, "prime %d\n", sieve);
+
+	while (read(fd, (void*)&readNum, sizeof(int)) != 0) {
+		if (readNum % sieve != 0) {
+			++primeCount;
+			if (primeCount == 1) {
+				if (pipe(primePipe) < 0)
 					exit(1);
-				else
-					pipe_flag = 1;
-				
-				if (!fork()) {
-					/* child */
-					redirect(R, write_pipe);
-					pipelineprime(R);
+
+				pipeCreateFlag = 1;
+
+				int pid = fork();
+				if (pid < 0)
+					exit(1);
+				else if (!pid) {
+					reDirect(R, primePipe);
+					pipelinePrime(R);
 					exit(0);
 				}
-				/* father */
-				redirect(W, write_pipe);
+				else {
+					reDirect(W, primePipe);
+				}
 			}
-			write(W, &prime, sizeof(int));		
+			write(W, (void*)&readNum, sizeof(int));
 		}
 	}
 	close(fd);
-	if (pipe_flag) {
+	if (pipeCreateFlag) {
 		close(W);
 		wait((int*)0);
 	}
