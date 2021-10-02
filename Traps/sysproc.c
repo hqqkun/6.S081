457d2434dcbcb20b01a2c96f52de6eb85319cc7a
阿昆 <37210982+hqqkun@users.noreply.github.com>
2021-10-02 02:27:51 -0500

    Add files via upload

---
+++ Traps/sysproc.c
@@ -0,0 +1,128 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  if(argint(0, &n) < 0)
+    return -1;
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  if(argaddr(0, &p) < 0)
+    return -1;
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  backTrace();
+  return 0;
+}
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+
+/* traps lab begin */
+uint64 sys_sigalarm(void) {
+    struct proc* p = myproc();
+    int ticks;
+    uint64 funcAddr;
+
+    if (argint(0, &ticks) < 0)
+        return -1;
+    if (argaddr(1, &funcAddr) < 0)
+        return -1;
+    if (ticks < 0) 
+        return -1;
+
+    p->alarmEn = ticks ? 1:0;
+    p->handlerInUse = 0;
+    p->ticks = 0;
+    p->alarmInterval = ticks;
+    p->handler = funcAddr;
+    return 0;
+}
+
+uint64 sys_sigreturn(void) {
+    struct proc* p = myproc();
+    *(p->trapframe) = *(p->intrFrame);
+    p->handlerInUse = 0;
+    return 0;
+}
+/* traps lab end */
